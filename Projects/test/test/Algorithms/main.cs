b9a8cfbea87454efd255c0d088b129562456e0b6
tripthilohit <tripthi.lohit@gmail.com>
2020-04-23 14:01:52 -0400

    unique nth character

--- Projects/test/test/Algorithms/main.cs
+++ Projects/test/test/Algorithms/main.cs
@@ -30,6 +30,7 @@ namespace test.Algo
             removeDupesFromArray.removeDupes(new int[] { 1, 1, 1, 7, 3, 3, 4, 5, 6 });
             mostRepeatedNumberinArray.mostRepeatedNumber(new int[] { 1, 2, 5, 3, 21, 8, 22, 21, 1, 1, 1, 1, 1 });
             palindromeString.palindrome("hello");
+            nthUniqueCharacterInString.nthUniqueChar("textg", 1);
         }
     }
 }
---
+++ Projects/test/test/Algorithms/nthUniqueCharacterInString.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test.Algo
+{
+    public class nthUniqueCharacterInString
+    {
+        public static void nthUniqueChar(String str, int n)
+        {
+            //Console.WriteLine(str.Length);
+            List<String> newArr = new List<String>();
+            List<String> comArr = new List<String>();
+            bool flag;
+
+
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                flag = true;
+                for (int j = i + 1; j < str.Length; j++)
+                {
+                    if (str[i] == str[j] || comArr.Contains(str[i].ToString()))
+                    {
+
+                        flag = false;
+                        comArr.Add(str[i].ToString());                       
+
+                    }
+                }
+                if (flag)
+                {
+             
+                    newArr.Add(str[i].ToString());
+                }
+            }
+
+            if (!newArr.Contains(str[str.Length - 1].ToString()))
+                newArr.Add(str[str.Length - 1].ToString());
+
+       
+
+            Console.WriteLine("The unique nth character is: " + newArr[n - 1]);
+
+        }
+    }
+}
