88ca75ebd1726893b1d156dcfa80844126813e65
tripthilohit <tripthi.lohit@gmail.com>
2020-04-23 00:53:58 -0400

    dictionary print

---
+++ Projects/test/test/dictionaryTests/printDictionary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace dictionaryTests
+{
+    public class printDictionary
+    {
+
+        public class EmptyClass
+        {
+
+            public static void Main(String[] args)
+            {
+                Dictionary<int, string> dict = new Dictionary<int, string>();
+
+                dict.Add(1, "one");
+                dict.Add(2, "two");
+
+                foreach (KeyValuePair<int, string> item in dict)
+                {
+                    Console.WriteLine("Key: {0}, Value: {1}", item.Key, item.Value);
+                }
+
+            }
+        }
+
+    }
+}
